a1c3bbaca498146fc36c100d3ba004abcc7a7a27
chewzzz <chewziqing@gmail.com>
2023-10-22 19:25:09 +0800

    view controller

---
+++ taco-cloud/src/main/java/sia/tacocloud/tacos/web/WebConfig.java
@@ -0,0 +1,16 @@
+package sia.tacocloud.tacos.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// view controller: forward the request to view
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+
+}
