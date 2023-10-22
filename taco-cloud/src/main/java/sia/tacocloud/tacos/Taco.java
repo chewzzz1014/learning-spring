90e0cfe15d4967530cde2eb6882f70914f0a40c0
chewzzz <chewziqing@gmail.com>
2023-10-22 16:28:33 +0800

    add fields validation

--- taco-cloud/src/main/java/sia/tacocloud/tacos/Taco.java
+++ taco-cloud/src/main/java/sia/tacocloud/tacos/Taco.java
@@ -1,4 +1,6 @@
 package sia.tacocloud.tacos;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
@@ -6,7 +8,12 @@ import java.util.List;
 @Data
 public class Taco {
 
+    @NotNull
+    @Size(min=5, message = "Name must be at least 5 characters long")
     private String name;
+
+    @NotNull
+    @Size(min=1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 
 }
