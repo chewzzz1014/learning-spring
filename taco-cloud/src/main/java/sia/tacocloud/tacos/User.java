a96b3b58dc17086376b9e3fa1e9cc15fbe9bca2d
chewzzz <chewziqing@gmail.com>
2023-10-25 14:55:09 +0800

    spring security

---
+++ taco-cloud/src/main/java/sia/tacocloud/tacos/User.java
@@ -0,0 +1,34 @@
+package sia.tacocloud.tacos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Data
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@RequiredArgsConstructor
+public class User implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private final String username;
+    private final String password;
+    private final String fullname;
+    private final String street;
+    private final String city;
+    private final String state;
+    private final String zip;
+    private final String phoneNumber;
+
+}
---
+++ taco-cloud/src/main/java/sia/tacocloud/tacos/security/UserDetailsService.java
@@ -0,0 +1,10 @@
+package sia.tacocloud.tacos.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface UserDetailsService {
+
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+
+}
