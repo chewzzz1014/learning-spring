0dc97ef4eae1c7c2cc73d996272452cfed6c76cd
chewzzz <chewziqing@gmail.com>
2023-10-30 23:21:55 +0800

    initial commit

---
+++ hello-world/src/main/java/com/apress/prospring5/ch02/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.apress.prospring5.ch02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
---
+++ hello-world/src/test/java/com/apress/prospring5/ch02/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apress.prospring5.ch02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
