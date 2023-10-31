9925afbdd6d56241e59cdf5850bcf436cdf93e63
chewzzz <chewziqing@gmail.com>
2023-10-31 21:52:14 +0800

    post router; http req file

--- content-calender/src/main/java/com/chewzzz/contentcalender/controller/ContentController.java
+++ content-calender/src/main/java/com/chewzzz/contentcalender/controller/ContentController.java
@@ -3,11 +3,12 @@ package com.chewzzz.contentcalender.controller;
 import com.chewzzz.contentcalender.model.Content;
 import com.chewzzz.contentcalender.respository.ContentCollectionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
@@ -20,9 +21,21 @@ public class ContentController {
         this.repository = repository;
     }
 
-    @GetMapping
+    @GetMapping("")
     public List<Content> findAll() {
         return repository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id) {
+        return repository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatusCode.valueOf(404), "Content Not Found")
+        );
+    }
+
+    @PostMapping("")
+    public void create(@RequestBody Content content) {
+        System.out.println(content.toString());
+        repository.save(content);
+    }
 }
--- content-calender/src/main/java/com/chewzzz/contentcalender/respository/ContentCollectionRepository.java
+++ content-calender/src/main/java/com/chewzzz/contentcalender/respository/ContentCollectionRepository.java
@@ -1,8 +1,12 @@
 package com.chewzzz.contentcalender.respository;
 
 import com.chewzzz.contentcalender.model.Content;
+import com.chewzzz.contentcalender.model.Status;
+import com.chewzzz.contentcalender.model.Type;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,20 +14,37 @@ import java.util.Optional;
 @Repository
 public class ContentCollectionRepository {
 
-    private final List<Content> content = new ArrayList<>();
+    private final List<Content> contentList = new ArrayList<>();
 
     public ContentCollectionRepository() {
 
     }
 
     public List<Content> findAll() {
-        return content;
+        return contentList;
     }
 
     public Optional<Content> findById(Integer id) {
-        return content.stream().filter(c -> c.id().equals(id)).findFirst();
+        return contentList.stream().filter(c -> c.id().equals(id)).findFirst();
     }
 
-    
+    @PostConstruct
+    private void init() {
+        // executed after dependency injection is done to perform any initialization (after DI in ContentController constructor)
+        Content c = new Content(
+                        1,
+                        "My First Blog Post",
+                        "My first blog post",
+                        Status.IDEA,
+                        Type.ARTICLE,
+                        LocalDateTime.now(),
+                        null,
+                        ""
+                    );
+        contentList.add(c);
+    }
 
+    public void save(Content c) {
+        contentList.add(c);
+    }
 }
