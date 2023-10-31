a0893204e318c8304060c1a5af7d1fe7f05605fd
chewzzz <chewziqing@gmail.com>
2023-10-31 12:07:29 +0800

    chap 2 getting started

---
+++ hello-world/src/main/java/com/apress/prospring5/ch02/decoupled/HelloWorldMessageProvider.java
@@ -0,0 +1,8 @@
+package com.apress.prospring5.ch02.decoupled;
+
+public class HelloWorldMessageProvider implements MessageProvider{
+    @Override
+    public String getMessage() {
+        return "Hello World!";
+    }
+}
