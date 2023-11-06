4e4a9544c9b7bb85082302d195b9bb139fbc4944
chewzzz <chewziqing@gmail.com>
2023-11-06 18:02:08 +0800

    rename description column to desc

--- content-calender/src/main/resources/schema.sql
+++ content-calender/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS Content(
     id INTEGER AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
-    description text,
+    desc text,
     status VARCHAR(20) NOT NULL,
     content_type VARCHAR(50) NOT NULL,
     date_created TIMESTAMP NOT NULL,
@@ -10,6 +10,6 @@ CREATE TABLE IF NOT EXISTS Content(
     PRIMARY KEY (id)
 );
 
-INSERT INTO Content(title, description, status, content_type, date_created)
+INSERT INTO Content(title, desc, status, content_type, date_created)
     VALUES('My Spring Data Blog Post', 'A post about spring data', 'IDEA', 'ARTICLE', CURRENT_TIMESTAMP);
 
