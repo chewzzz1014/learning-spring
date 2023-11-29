4bc15f513c80ff40bd8155aa523e9a22c22cbfa0
chewzzz <chewziqing@gmail.com>
2023-11-29 09:50:53 +0800

    initial commit

---
+++ db-demo/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO widgets(id, name, purpose) VALUES
+(1, 'Widget A', 'Used for testing purposes.');
+
+INSERT INTO widgets (id, name, purpose) VALUES
+(2, 'Widget B', 'Designed for entertainment.');
+
+INSERT INTO widgets (id, name, purpose) VALUES
+(3, 'Widget C', 'Enhances productivity.');
+
+INSERT INTO widgets (id, name, purpose) VALUES
+(4, 'Widget D', 'Perfect for outdoor activities.');
+
+INSERT INTO widgets (id, name, purpose) VALUES
+(5, 'Widget E', 'Improves overall well-being.');
