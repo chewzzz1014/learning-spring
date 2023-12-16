3e3f9cb2c68fe2d0dcd49e9af16022657eb68a3d
chewzzz <chewziqing@gmail.com>
2023-12-16 16:02:26 +0800

    ways to create bean

--- gsws_ap_b_ex1/src/main/java/main/Main.java
+++ gsws_ap_b_ex1/src/main/java/main/Main.java
@@ -4,16 +4,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
         // get the bean by bean name (method name in config)
-        Parrot p = context.getBean("parrot2", Parrot.class);
+        Parrot p = context.getBean(Parrot.class);
+        System.out.println(p);
         System.out.println(p.getName());
 
-        String s = context.getBean(String.class);
-        System.out.println(s);
-
-        Integer n = context.getBean(Integer.class);
-        System.out.println(n);
+//        String s = context.getBean(String.class);
+//        System.out.println(s);
+//
+//        Integer n = context.getBean(Integer.class);
+//        System.out.println(n);
     }
 }
