5a90f613493d6d97f44da8f99a5f36c760cad06f
chewzzz <chewziqing@gmail.com>
2023-12-22 10:18:20 +0800

    autowired spring scope

---
+++ spring-start-here/sq-ch5-ex6/sq-ch5-ex6/src/main/java/com/example/sqch5ex6/SqCh5Ex6Application.java
@@ -0,0 +1,13 @@
+package com.example.sqch5ex6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh5Ex6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh5Ex6Application.class, args);
+	}
+
+}
---
+++ spring-start-here/sq-ch5-ex6/sq-ch5-ex6/src/test/java/com/example/sqch5ex6/SqCh5Ex6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqch5ex6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh5Ex6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
