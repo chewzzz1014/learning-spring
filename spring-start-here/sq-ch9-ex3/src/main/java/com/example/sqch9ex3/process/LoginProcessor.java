80a8073f7025166d5f26db969fded22d2fcef3b2
chewzzz <chewziqing@gmail.com>
2023-12-26 16:25:31 +0800

    application scoped bean

---
+++ spring-start-here/sq-ch9-ex3/src/main/java/com/example/sqch9ex3/process/LoginProcessor.java
@@ -0,0 +1,52 @@
+package com.example.sqch9ex3.process;
+
+import com.example.sqch9ex3.service.LoggedUserManagementService;
+import com.example.sqch9ex3.service.LoginCountService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public class LoginProcessor {
+
+    private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
+
+    private String username;
+    private String password;
+
+    public LoginProcessor(LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
+        this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
+    }
+
+    public boolean login() {
+        loginCountService.increment();
+
+        String username = this.getUsername();
+        String password = this.getPassword();
+
+        boolean loginResult = false;
+        if ("chewzzz".equals(username) && "password".equals(password)) {
+            loginResult = true;
+            loggedUserManagementService.setUsername(username); // session scoped
+        }
+        return loginResult;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
