652c831a313c02663420fc26f5f48a3fd56fa812
chewzzz <chewziqing@gmail.com>
2024-01-01 22:14:37 +0800

    request body

--- spring-start-here/sq-ch10-ex5/src/main/java/com/example/sqch10ex5/controller/PaymentController.java
+++ spring-start-here/sq-ch10-ex5/src/main/java/com/example/sqch10ex5/controller/PaymentController.java
@@ -7,22 +7,27 @@ import com.example.sqch10ex5.service.PaymentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 public class PaymentController {
 
     private final PaymentService paymentService;
+    private static Logger logger = Logger.getLogger(PaymentController.class.getName());
 
     public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
     @PostMapping("/payment")
-    public ResponseEntity<?> makePayment() {
-            PaymentDetails paymentDetails = paymentService.processPayment();
-            return ResponseEntity
-                    .status(HttpStatus.ACCEPTED)
-                    .body(paymentDetails);
+    public ResponseEntity<PaymentDetails> makePayment(@RequestBody PaymentDetails paymentDetails) {
+        logger.info("Received payment " + paymentDetails.getAmount());
+
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(paymentDetails);
     }
 }
