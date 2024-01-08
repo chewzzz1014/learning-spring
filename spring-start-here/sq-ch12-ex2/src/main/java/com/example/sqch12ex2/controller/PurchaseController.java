e1a4aa6ba62161b77f8a51c4d4552be483700833
chewzzz <chewziqing@gmail.com>
2024-01-08 16:49:07 +0800

    connect to postgres running on docker-compose

---
+++ spring-start-here/sq-ch12-ex2/src/main/java/com/example/sqch12ex2/controller/PurchaseController.java
@@ -0,0 +1,28 @@
+package com.example.sqch12ex2.controller;
+
+import com.example.sqch12ex2.model.Purchase;
+import com.example.sqch12ex2.repository.PurchaseRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+
+    private final PurchaseRepository purchaseRepository;
+
+    public PurchaseController(PurchaseRepository purchaseRepository) {
+        this.purchaseRepository = purchaseRepository;
+    }
+
+    @PostMapping
+    public void storePurchase(@RequestBody Purchase purchase) {
+        purchaseRepository.storePurchase(purchase);
+    }
+
+    @GetMapping
+    public List<Purchase> findPurchases() {
+        return purchaseRepository.findAllPurchases();
+    }
+}
