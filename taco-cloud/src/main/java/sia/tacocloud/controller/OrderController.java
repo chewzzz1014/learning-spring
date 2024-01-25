ac150e2c36caaca65ca3d4335a24717e980fc8ff
chewzzz <chewziqing@gmail.com>
2024-01-25 14:06:22 +0800

    configuration properties

--- taco-cloud/src/main/java/sia/tacocloud/controller/OrderController.java
+++ taco-cloud/src/main/java/sia/tacocloud/controller/OrderController.java
@@ -2,8 +2,12 @@ package sia.tacocloud.controller;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,17 +17,28 @@ import org.springframework.web.bind.support.SessionStatus;
 import sia.tacocloud.model.TacoOrder;
 import sia.tacocloud.repository.OrderRepository;
 import sia.tacocloud.security.User;
+import sia.tacocloud.utils.OrderProps;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
+@ConfigurationProperties(prefix = "taco.orders")
 public class OrderController {
 
     private OrderRepository orderRepository;
+    private OrderProps orderProps;
 
-    public OrderController(OrderRepository orderRepository) {
+    public OrderController(OrderRepository orderRepository, OrderProps orderProps) {
         this.orderRepository = orderRepository;
+        this.orderProps = orderProps;
+    }
+
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, orderProps.getPageSize());
+        model.addAttribute("orders", orderRepository.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
     }
 
     @GetMapping("/current")
--- taco-cloud/src/main/java/sia/tacocloud/repository/OrderRepository.java
+++ taco-cloud/src/main/java/sia/tacocloud/repository/OrderRepository.java
@@ -2,10 +2,14 @@ package sia.tacocloud.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import sia.tacocloud.model.TacoOrder;
+import sia.tacocloud.security.User;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 
     List<TacoOrder> findByDeliveryZip(String deliveryZip);
+
+    List<TacoOrder> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
