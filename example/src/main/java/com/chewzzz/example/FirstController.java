81db56a31200476bfeb1b83ebbdaade829d8a96d
chewzzz <chewziqing@gmail.com>
2024-04-29 21:00:16 +0800

    CRUD

--- example/src/main/java/com/chewzzz/example/FirstController.java
+++ example/src/main/java/com/chewzzz/example/FirstController.java
@@ -3,17 +3,51 @@ package com.chewzzz.example;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class FirstController {
+
+    private final StudentRepository studentRepository;
+
+    public FirstController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello from my first controller";
     }
 
+    @GetMapping("/students")
+    public List<Student> findAllStudent() {
+        return studentRepository.findAll();
+    }
+
+    @GetMapping("/students/search/{student-name}")
+    public List<Student> findStudentByName(@PathVariable("student-name") String name) {
+        return studentRepository.findAllByFirstnameContaining(name);
+    }
+
+    @GetMapping("/students/{student-id}")
+    public Student findStudentById(@PathVariable("student-id") Integer id) {
+        return studentRepository.findById(id).orElse(new Student());
+    }
+
     @PostMapping("/post")
-    public String post(@RequestBody String message) {
+    public String postMessage(@RequestBody String message) {
         return "Request accepted. Message accepted is: " + message;
     }
 
+    @PostMapping("/students")
+    public Student postStudent(@RequestBody Student student) {
+        studentRepository.save(student);
+        return student;
+    }
+
+    @DeleteMapping("/students/{student-id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteStudent(@PathVariable("student-id") Integer id) {
+        studentRepository.deleteById(id);
+    }
 }
